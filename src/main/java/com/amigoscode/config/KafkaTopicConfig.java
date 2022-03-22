f4aa26aa9cea3b1ca1b44414031bddac6d7e4b0b
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-03-22 16:17:14 +0300

    Initial commit

---
+++ src/main/java/com/amigoscode/config/KafkaTopicConfig.java
@@ -0,0 +1,13 @@
+package com.amigoscode.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    public NewTopic amigoscodeTopic() {
+
+    }
+}
